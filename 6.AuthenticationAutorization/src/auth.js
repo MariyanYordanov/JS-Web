8610bf1fa87a44c47a2b333a69424947a3a496ca
Mariyan Yordanov <marsrewq@gmail.com>
2024-08-08 02:20:41 +0300

    Refactor authentication and authorization code

--- 6.AuthenticationAutorization/src/auth.js
+++ 6.AuthenticationAutorization/src/auth.js
@@ -1,17 +1,25 @@
-const users = {};
+const users = {
+    'pepi': {
+        username: 'pepi',
+        password: '1234',
+    },
+};
 
 function registerUser(username, password) {
-  if (users[username]) {
-    throw new Error('User already exists');
-  }
-  users[username] = password;
-}
+    if (users[username]) {
+        throw new Error("User already exists");
+    }
+    const user = { username, password };
+    users[username] = user;
+    return user;
+};
 
 function loginUser(username, password) {
-  if (users[username] === password) {
-    return true;
-  }
-  return false;
-}
+    const user = users[username];
+    if (!user || user.password !== password) {
+        throw new Error("Incorrect username or paswword");
+    }
+    return user;
+};
 
-module.exports = {  registerUser, loginUser };
+module.exports = { registerUser, loginUser };
--- 6.AuthenticationAutorization/src/index.js
+++ 6.AuthenticationAutorization/src/index.js
@@ -1,55 +1,55 @@
-const express = require('express');
-const handlebars = require('express-handlebars');
-const session = require('express-session'); 
+const express = require("express");
+const handlebars = require("express-handlebars");
+const session = require("express-session");
 
 const app = express();
 const port = 3000;
-const secret = 'mysecret';
+const secret = "mysecret";
 
 const hbs = handlebars.create({
-    extname: 'hbs',
+    extname: "hbs",
 });
 
-app.engine('hbs', hbs.engine);
-app.set('view engine', 'hbs');
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
 
 app.use(express.urlencoded({ extended: true }));
-app.use(session({
-    secret,
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: false }
-}));
+app.use(
+    session({
+        secret,
+        resave: false,
+        saveUninitialized: true,
+        cookie: { secure: false },
+    })
+);
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
     const user = req.session.user;
-   console.log(user); 
-    res.render('home', { user });
+    console.log(user);
+    res.render("home", { user });
 });
 
-app.get('/login', (req, res) => {
+app.get("/login", (req, res) => {
     const error = req.session.error;
     delete req.session.error;
-
-    res.render('login', { error });
+    res.render("login", { error });
 });
 
-app.post('/login', (req, res) => {
+app.post("/login", (req, res) => {
     console.log(req.body);
     const { username, password } = req.body;
-    if(username != 'pepi' || password != '1234'){
-        req.session.error = 'Invalid username or password';
-        res.redirect('/login');
+    if (username != "pepi" || password != "1234") {
+        req.session.error = "Invalid username or password";
+        res.redirect("/login");
         return;
-    } 
-
-    req.session.user = 'pepi';
-    res.redirect('/');
+    }
+    req.session.user = "pepi";
+    res.redirect("/");
 });
 
-app.get('/logout', (req, res) => {
+app.get("/logout", (req, res) => {
     req.session.destroy();
-    res.redirect('/');
+    res.redirect("/");
 });
 
 app.listen(port, () => {
