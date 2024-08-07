d159a623f47d289727ecc3e61dcfdc9ec71508ee
Mariyan Yordanov <marsrewq@gmail.com>
2024-08-07 08:17:53 +0300

    Add cookie setting functionality to home page

--- 5.SessionCookies/src/index.js
+++ 5.SessionCookies/src/index.js
@@ -17,6 +17,11 @@ app.get('/', (req, res) => {
     res.render('home', { title: 'Home' });
     });
 
+app.get('/set', (req, res) => {
+    res.setHeader('Set-Cookie', 'cookieDemo=Hello; HttpOnly');
+    res.redirect('/');
+    });
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
