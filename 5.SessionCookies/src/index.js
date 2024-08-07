5cdfb6494f7ab0f76ce55162d7c0f74c370587e6
Mariyan Yordanov <marsrewq@gmail.com>
2024-08-07 17:24:04 +0300

    Update session cookie handling and add theme support

--- 5.SessionCookies/src/index.js
+++ 5.SessionCookies/src/index.js
@@ -1,27 +1,53 @@
-const express = require('express');
-const handlebars = require('express-handlebars');
+const express = require("express");
+const handlebars = require("express-handlebars");
 
 const app = express();
 const port = 3000;
 
-app.use('static',express.static('static'));
+app.use("/static", express.static("static"));
 app.use(express.urlencoded({ extended: true }));
 
 const hbs = handlebars.create({
-    extname: '.hbs',
+    extname: ".hbs",
 });
-app.engine('hbs', hbs.engine);
-app.set('view engine', 'hbs');
 
-app.get('/', (req, res) => {
-    res.render('home', { title: 'Home' });
-    });
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
 
-app.get('/set', (req, res) => {
-    res.setHeader('Set-Cookie', 'cookieDemo=Hello; HttpOnly');
-    res.redirect('/');
-    });
+app.get("/", (req, res) => {
+    const cookieData = req.headers["cookie"];
+    const cookies = Object.fromEntries(
+        cookieData
+            .split(";")
+            .map(kvp => kvp.trim())
+            .filter(kvp => kvp)
+            .map(kvp => kvp.split("="))
+    );
+    const theme = cookies.theme == "dark";
+    res.render('home', { theme, title: "Home" });
+});
+
+app.get("/set", (req, res) => {
+    res.setHeader("Set-Cookie", "cookieDemo=Hello; HttpOnly");
+    res.redirect("/");
+});
+
+app.get("/get", (req, res) => {
+    const cookie = req.headers["cookie"];
+    console.log(cookie);
+    res.render('get', { title: "Get" });
+});
+
+app.get("/use-light", (req, res) => {
+    res.setHeader("Set-Cookie", "theme=light;");
+    res.redirect("/");
+});
+
+app.get("/use-dark", (req, res) => {
+    res.setHeader("Set-Cookie", "theme=dark; HttpOnly");
+    res.redirect("/");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Example app listening at http://localhost:${port}`);
 });
