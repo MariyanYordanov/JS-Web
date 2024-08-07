8dc454e1d8e23d6add9ceb2c24367aa91161b7f1
Mariyan Yordanov <marsrewq@gmail.com>
2024-08-07 06:15:46 +0300

    Update .DS_Store file

---
+++ 2.NodeIntro/util.js
@@ -0,0 +1,15 @@
+function print(msg){
+    console.log(msg);
+}
+
+function add(a,b){
+    return a+b;
+    
+};
+
+const data = [1,2,3,4,5,6,7,8,9,10];
+module.exports = {
+    print,
+    add,
+    data
+};
