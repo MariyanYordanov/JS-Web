15b14216f739a69190968008d196bb187a11ee87
Mariyan Yordanov <marsrewq@gmail.com>
2024-08-12 08:23:53 +0300

    Update home.js and index.js for form validation

--- 7.Verification/src/controllers/home.js
+++ 7.Verification/src/controllers/home.js
@@ -1,14 +1,31 @@
-const { Router } = require('express');
+const { Router } = require("express");
+const { body, validationResult } = require("express-validator");
 
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.render('home', {
-        title: 'Home Page',
-        message: 'Validation Demo'
+router.get("/", (req, res) => {
+    res.render("home", {
+        title: "Home Page",
+        message: "Validation Demo",
     });
 });
 
+router.post(
+    "/",
+    body("email").isEmail().withMessage("Invalid email"),
+    body("password")
+        .isLength({ min: 5 })
+        .withMessage("Password must be at least 5 characters long"),
+    (req, res) => {
+        const { errors } = validationResult(req);
+        if (errors.length) {
+            res.render('home', { errors: { message: 'Invalid something'} });
+            return;
+        }
+        res.redirect('/');
+    }
+);
+
 module.exports = {
-    router
-}
+    router,
+};
--- 7.Verification/src/index.js
+++ 7.Verification/src/index.js
@@ -6,6 +6,7 @@ const app = express();
 const port = 3000;
 
 hbsConfig(app);
+app.use(express.urlencoded({ extended: true }));
 app.use(router);
 
 app.listen(port, () => {
