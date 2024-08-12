189ae7b5fa6e1f373a82fecb033cecc858f72c44
Mariyan Yordanov <marsrewq@gmail.com>
2024-08-12 09:50:15 +0300

    Refactor form validation and error handling

--- 7.Verification/src/controllers/home.js
+++ 7.Verification/src/controllers/home.js
@@ -12,18 +12,31 @@ router.get("/", (req, res) => {
 
 router.post(
     "/",
-    body("email").isEmail().withMessage("Invalid email"),
+    body("email")
+        .trim()
+        .isEmail()
+        .normalizeEmail()
+        .withMessage("Invalid email"),
     body("password")
+        .trim()
         .isLength({ min: 5 })
         .withMessage("Password must be at least 5 characters long"),
+    body('repass')
+        .trim()
+        .custom((value, { req }) => {
+            if (value !== req.body.password) {
+                throw new Error('Passwords do not match');
+            }
+            return true;
+        }),
     (req, res) => {
         const result = validationResult(req);
-        const errors = result.errors.map((error) => error.msg); 
+        const errors = Object.fromEntries(result.errors.map((e) => [e.path, e.msg]));
 
         console .log(errors);
 
-        if (errors.length) {
-            res.render('home', { errors});
+        if (result.errors.length) {
+            res.render('home', { data: req.body, errors });
             return;
         }
         res.redirect('/');
